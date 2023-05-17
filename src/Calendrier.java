3835ec8f0fff7eaa3db7cb6b46bceeb7f67a1b5a
Abdellahbado <la_boukouffallah@esi.dz>
2023-05-17 18:54:55 +0100

    added an exemple when we have two days and the task will be divided among them

--- src/Calendrier.java
+++ src/Calendrier.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Calendrier {
     private TreeSet<Jour> listeJours;
@@ -7,11 +8,13 @@ public class Calendrier {
     public Calendrier(TreeSet<Jour> listeJours) {
         this.listeJours = listeJours;
     }
+
     public void afficherListeJours() {
         for (Jour jour : listeJours) {
             jour.afficher();
         }
     }
+
     public boolean plannifierTacheAuto(Tache tache) {
         Iterator<Jour> iterator = this.listeJours.iterator();
         while (iterator.hasNext()) {
@@ -20,23 +23,27 @@ public class Calendrier {
         }
         return false;
     }
-    public boolean plannifierTacheAuto(Tache tache, LocalDate dateLimit){
+
+    public boolean plannifierTacheAuto(Tache tache, LocalDate dateLimit) {
         Iterator<Jour> iterator = this.listeJours.iterator();
-        while ((iterator.hasNext())&&(iterator.next().getDate().compareTo(dateLimit)<0)){
+        while ((iterator.hasNext()) && (iterator.next().getDate().compareTo(dateLimit) < 0)) {
             Jour jour = iterator.next();
             if (jour.plannifierTacheAuto(tache)) return true;
         }
         return false;
     }
-    public boolean plannifierTacheDecomp (TacheDecomposable tache){
+
+    public boolean plannifierTacheDecomp(TacheDecomposable tache) {
         Iterator<Jour> iterator = this.listeJours.iterator();
         // on parcours les jours et pour chaque jour on appelle plannifierDecomp
         // si elle retourne une tache donc il ya encore une tahce qui n'a pas eté programmée
         TacheDecomposable t1 = tache;
+
         while (iterator.hasNext()) {
             Jour jour = iterator.next();
-            t1 = jour.plannifierTacheDecomp(tache);
-            if(t1 == null) return true; // la tache a ete planifier
+            t1.afficher();
+            t1 = jour.plannifierTacheDecomp(t1);
+            if (t1 == null) return true; // la tache a ete planifier
         }
         return false;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,35 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        TacheDecomposable tacheDecomposable = new TacheDecomposable("TP POO", 300, Priorite.Medium, LocalDate.of(2023, 5, 25), Categorie.STUDIES, EtatAvancement.notRealised, "TP POO 1", 1);
+        TacheDecomposable tacheDecomposable = new TacheDecomposable("TP POO", 480, Priorite.Medium, LocalDate.of(2023, 5, 25), Categorie.STUDIES, EtatAvancement.notRealised, "TP POO 1", 1);
 
         Creneau creneau1 = new Creneau(LocalTime.of(9, 0), LocalTime.of(10, 0), EtatCreneau.Libre, null);
         Creneau creneau2 = new Creneau(LocalTime.of(10, 0), LocalTime.of(11, 0), EtatCreneau.Libre, null);
         Creneau creneau3 = new Creneau(LocalTime.of(13, 0), LocalTime.of(14, 0), EtatCreneau.Libre, null);
         Creneau creneau4 = new Creneau(LocalTime.of(14, 0), LocalTime.of(15, 0), EtatCreneau.Libre, null);
+        Creneau creneau5 = new Creneau(LocalTime.of(9, 0), LocalTime.of(10, 0), EtatCreneau.Libre, null);
+        Creneau creneau6 = new Creneau(LocalTime.of(10, 0), LocalTime.of(11, 0), EtatCreneau.Libre, null);
+        Creneau creneau7 = new Creneau(LocalTime.of(13, 0), LocalTime.of(14, 0), EtatCreneau.Libre, null);
+        Creneau creneau8 = new Creneau(LocalTime.of(14, 0), LocalTime.of(15, 0), EtatCreneau.Libre, null);
         TreeSet<Creneau> listCreneau = new TreeSet<>();
         listCreneau.add(creneau1);
         listCreneau.add(creneau2);
         listCreneau.add(creneau3);
         listCreneau.add(creneau4);
-        Jour jour =  new Jour(LocalDate.of(2023,5,17),listCreneau);
+        TreeSet<Creneau> listCreneau2 = new TreeSet<>();
+        listCreneau2.add(creneau5);
+        listCreneau2.add(creneau6);
+        listCreneau2.add(creneau7);
+        listCreneau2.add(creneau8);
+        Jour jour = new Jour(LocalDate.of(2023, 5, 17), listCreneau);
+        Jour jour1 = new Jour(LocalDate.of(2023, 5, 18), listCreneau2);
+
         TreeSet<Jour> listeJours = new TreeSet<>();
         listeJours.add(jour);
+        listeJours.add(jour1);
         Calendrier calendrier = new Calendrier(listeJours);
         boolean bool = calendrier.plannifierTacheDecomp(tacheDecomposable);
         calendrier.afficherListeJours();
-        System.out.println("Boolean = "+bool);
+        System.out.println("Boolean = " + bool);
     }
 }
